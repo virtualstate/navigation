81cd58833bf0840e1e0807a3e5b6698133b2561c
Fabian Cook <hello@fabiancook.dev>
2021-12-21 11:02:18 +1300

    attempt to test packaged version

--- src/tests/app-history.imported.ts
+++ src/tests/app-history.imported.ts
@@ -1,7 +1,7 @@
-import {AppHistory} from "../app-history";
+import {AppHistory} from "@virtualstate/app-history";
 import {AppHistoryAssertFn, assertAppHistory} from "./app-history";
 
 const input = () => new AppHistory();
 const fn: AppHistoryAssertFn = await assertAppHistory(input);
 fn(input);
-console.log("PASS assertAppHistory:local:new AppHistory");
+console.log("PASS assertAppHistory:import:new AppHistory");
--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,4 +1,5 @@
 import "./app-history.class";
+import "./app-history.imported";
 
 // TODO add checks to ensure everything is closed
 process.exit(0);
