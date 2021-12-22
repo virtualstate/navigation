55db76272d64fb2677a6f555574aef1f1a61cb79
Fabian Cook <hello@fabiancook.dev>
2021-12-22 23:18:56 +1300

    add deno tests to actions

---
+++ src/deno-uuid.ts
@@ -0,0 +1,5 @@
+import { generate } from "deno:std/uuid/v4";
+
+export function v4() {
+    return generate();
+}
---
+++ src/tests/performance.ts
@@ -0,0 +1,3 @@
+const { PerformanceObserver } = await import("perf_hooks").catch(() => ({ PerformanceObserver: undefined }));
+
+export default PerformanceObserver;
