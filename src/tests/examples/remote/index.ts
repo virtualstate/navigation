f8e7d4be8371bf0fddeac839b9e06b4c3080581b
Fabian Cook <hello@fabiancook.dev>
2021-12-24 12:56:24 +1300

    remote initial example

---
+++ src/tests/examples/remote/index.ts
@@ -0,0 +1,38 @@
+import {AppHistory as AppHistorySpec, AppHistoryEntry} from "../../../spec/app-history";
+import {AppHistory} from "../../../app-history";
+import {assert, ok} from "../../util";
+
+// This is the extremely one to one way.. with no consideration for how these
+// navigations happened
+//
+// Also doesn't abstract between the two with a transport later like fetch, we will
+// show that later on
+export async function remoteExample(appHistory: AppHistorySpec) {
+    const remote = new AppHistory();
+
+    appHistory.addEventListener("currentchange", async () => {
+        const { current } = appHistory;
+        if (!current) return;
+        await remote.navigate(
+            current.url,
+            {
+                state: current.getState()
+            }
+        ).finished;
+    });
+
+    const expectedUrl = `/${Math.random()}`;
+    const expectedState = `${Math.random()}`;
+    await appHistory.navigate(expectedUrl, {
+        state: {
+            [expectedState]: true
+        }
+    }).finished;
+
+    const { current } = remote;
+
+    assert<AppHistoryEntry>(current);
+    ok(current.url === expectedUrl);
+    const state = current.getState<Record<string, unknown>>();
+    assert(state[expectedState] === true);
+}
