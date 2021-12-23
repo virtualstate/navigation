31da1c3fe20a10e52b06bfa116b4dc56636b57b5
Fabian Cook <hello@fabiancook.dev>
2021-12-24 12:44:54 +1300

    Utilise named AbortError

--- src/app-history-errors.ts
+++ src/app-history-errors.ts
@@ -1,17 +1,31 @@
+export interface AbortError extends Error {
+    // Shown here
+    // https://dom.spec.whatwg.org/#aborting-ongoing-activities
+    // https://webidl.spec.whatwg.org/#aborterror
+    name: "AbortError";
+}
+
+export class AbortError extends Error {
+    name = "AbortError" as const
+}
+
+export function isAbortError(error: unknown): error is AbortError {
+    return error instanceof Error && error.name === "AbortError"
+}
+
+export interface InvalidStateError extends Error {
+    // Following how AbortError is named
+    name: "InvalidStateError";
+}
+
 export class InvalidStateError extends Error {
+    name = "InvalidStateError" as const;
+
     constructor(message?: string) {
         super(`InvalidStateError${message ? `: ${message}` : ""}`);
     }
 }
 
-export interface AbortError extends Error {
-
-}
-
-export class AbortError extends Error {
-    name = "AbortError"
-}
-
-export function isAbortError(error: Error): error is AbortError {
-    return error.name === "AbortError"
+export function isInvalidStateError(error: unknown): error is InvalidStateError {
+    return error instanceof Error && error.name === "AbortError"
 }
