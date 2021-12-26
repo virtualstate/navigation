57d1926cd24ff2c3ff8a7ec48169f32258d13d38
Fabian Cook <hello@fabiancook.dev>
2021-12-26 14:30:38 +1300

    planner

--- src/app-history-transition-planner.ts
+++ src/app-history-transition-planner.ts
@@ -1,16 +1,52 @@
-import {AppHistoryTransition} from "./app-history-transition";
+import {
+    AppHistoryTransition,
+    AppHistoryTransitionEntry,
+    AppHistoryTransitionWait,
+    AppHistoryTransitionWhile
+} from "./app-history-transition";
+import {AppHistory} from "./app-history";
+import {AppHistoryEntry} from "./app-history-entry";
+import {InvalidStateError} from "./app-history-errors";
+
 
 export interface AppHistoryTransitionPlannerOptions {
     transition: AppHistoryTransition;
+    currentPlan?: AppHistoryTransitionPlan;
+    constructAppHistory(): AppHistory;
 }
 
-export interface AppHistoryTransitionPlan {
+export const AppHistoryTransitionPlanAppHistorySymbol = Symbol.for("@virtualstate/app-history/transition/plan/appHistory")
+export const AppHistoryTransitionPlanWhile = Symbol.for("@virtualstate/app-history/transition/plan/while");
+export const AppHistoryTransitionPlanWait = Symbol.for("@virtualstate/app-history/transition/plan/wait");
 
+export interface AppHistoryTransitionPlan {
+    [AppHistoryTransitionPlanAppHistorySymbol]: AppHistory;
+    [AppHistoryTransitionPlanWhile](promise: Promise<unknown>): void;
+    [AppHistoryTransitionPlanWait](): Promise<AppHistoryEntry>;
+    transitions: AppHistoryTransition[];
+    known: Set<AppHistoryEntry>
+    knownTransitions: Set<AppHistoryTransition>;
+    resolve(): Promise<void>;
 }
 
 export function plan(options: AppHistoryTransitionPlannerOptions) {
-
-
-
-
+    const {
+        transition,
+        currentPlan,
+        constructAppHistory,
+    } = options;
+    const nextPlan: AppHistoryTransitionPlan = {
+        [AppHistoryTransitionPlanAppHistorySymbol]: currentPlan?.[AppHistoryTransitionPlanAppHistorySymbol] ?? constructAppHistory(),
+        transitions: [],
+        ...currentPlan,
+        knownTransitions: new Set(currentPlan?.knownTransitions ?? []),
+        [AppHistoryTransitionPlanWhile]: transition[AppHistoryTransitionWhile],
+        [AppHistoryTransitionPlanWait]: transition[AppHistoryTransitionWait]
+    };
+    if (nextPlan.knownTransitions.has(transition)) {
+        throw new InvalidStateError("Transition already found in plan, this may lead to unexpected behaviour, please raise an issue at ")
+    }
+    nextPlan.knownTransitions.add(transition);
+    nextPlan.transitions.push(transition);
+    return nextPlan;
 }
