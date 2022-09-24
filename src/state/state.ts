ae8bbe596546c554a6afa6d512f0dee143271af6
Fabian Cook <hello@fabiancook.dev>
2022-09-25 00:25:15 +1200

    bind state to navigation instance

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,4 +1,4 @@
-import {Navigation} from "../spec/navigation";
+import {NavigateEvent, Navigation} from "../spec/navigation";
 import {getNavigation} from "../get-navigation";
 import {Push} from "@virtualstate/promise";
 import {Event} from "../event-target";
@@ -14,7 +14,38 @@ export function getState<S>(navigation: Navigation<S> = getNavigation()) {
     return navigation.currentEntry.getState();
 }
 
-export async function * state<S>(navigation: Navigation<S> = getNavigation()) {
+const states = new WeakMap<Navigation, AsyncIterable<unknown>>();
+
+export function state<S>(navigation: Navigation<S> = getNavigation()) {
+
+    const existing = states.get(navigation);
+
+    // Using a stable object for a navigation instance
+    // allows for this state object to be used in memo'd functions
+    if (isExisting(existing)) {
+        return existing;
+    }
+
+    // Returning an object like this allows callers to bind
+    // a navigation to the stateGenerator function, each time
+    // a new asyncIterator is created, the transition or currentEntry
+    // at that point will be used, meaning that this object
+    // too can be freely used as a static source across
+    // an application.
+    const result: AsyncIterable<S> = {
+        [Symbol.asyncIterator]() {
+            return stateGenerator(navigation);
+        }
+    }
+    states.set(navigation, result);
+    return result;
+
+    function isExisting(existing: AsyncIterable<unknown>): existing is AsyncIterable<S> {
+        return !!existing;
+    }
+}
+
+export async function * stateGenerator<S>(navigation: Navigation<S> = getNavigation()) {
 
     let lastState: S | undefined = undefined,
         wasState = false;
@@ -30,6 +61,7 @@ export async function * state<S>(navigation: Navigation<S> = getNavigation()) {
 
     pushState();
 
+    navigation.addEventListener("navigatesuccess", onNavigateSuccess);
     navigation.addEventListener("navigateerror", onNavigateError, { once: true });
     navigation.addEventListener("currententrychange", pushState);
 
@@ -38,8 +70,8 @@ export async function * state<S>(navigation: Navigation<S> = getNavigation()) {
     yield * push;
 
     function pushState() {
-        if (navigation.currentEntry.key !== currentEntry.key) {
-            return;
+        if (navigation.currentEntry.id !== currentEntry.id) {
+            return close();
         }
         const state = currentEntry.getState()
         if (wasState || typeof state !== "undefined") {
@@ -52,24 +84,23 @@ export async function * state<S>(navigation: Navigation<S> = getNavigation()) {
         }
     }
 
+    function onNavigateSuccess() {
+        if (navigation.currentEntry.id !== currentEntry.id) {
+            close();
+        }
+    }
+
     function onNavigateError(event: Event) {
         const { error } = event;
         push.throw(error);
     }
 
     function close() {
+        navigation.removeEventListener("navigatesuccess", onNavigateSuccess);
         navigation.removeEventListener("navigateerror", onNavigateError);
         navigation.removeEventListener("currententrychange", pushState);
         currentEntry.removeEventListener("dispose", close);
-
-        // Note that this is breaking the state! Not closing
-        // If the state was not read by the time this is called
-        // it may not be available
-        //
-        // Retaining currentEntry and using
-        // currentEntry.getState() can always get
-        // the final state
-        push.break();
+        push.close();
     }
 
 }
--- src/tests/state/index.tsx
+++ src/tests/state/index.tsx
@@ -3,6 +3,7 @@ import {Router} from "../../routes";
 import {getNavigation} from "../../get-navigation";
 import {ok} from "../../is";
 import {descendants, h} from "@virtualstate/focus";
+import {Navigation} from "../../navigation";
 
 {
     const navigation = getNavigation()
@@ -16,10 +17,12 @@ import {descendants, h} from "@virtualstate/focus";
     const changes: unknown[] = []
 
     async function *Test() {
+        console.log("Test start");
         for await (const change of state()) {
             console.log({ change })
             changes.push(change);
         }
+        console.log("Test end");
     }
 
     route("/test",  () => (<Test />));
@@ -27,6 +30,7 @@ import {descendants, h} from "@virtualstate/focus";
     // This is detaching the promise from the route transition.
     then(node => void descendants(node).catch(error => error))
 
+    console.log("Navigate /test");
     await navigation.navigate("/test").finished;
 
     const initialEntry = navigation.currentEntry;
@@ -38,6 +42,9 @@ import {descendants, h} from "@virtualstate/focus";
 
     setState("Test 3");
 
+    await new Promise(resolve => setTimeout(resolve, 100));
+
+    console.log("Navigate /another");
     await navigation.navigate("/another").finished;
 
     const nextEntry = navigation.currentEntry;
@@ -55,5 +62,107 @@ import {descendants, h} from "@virtualstate/focus";
     ok(changes[1] === "Test 2");
     ok(changes[2] === "Test 3");
 
+
+}
+
+{
+
+    const navigation = new Navigation()
+
+    await navigation.navigate("/").finished;
+
+    const router = new Router(navigation);
+
+    const { route, then } = router;
+
+    const changes: unknown[] = []
+
+    const entryChange = state(navigation);
+
+    async function *Test() {
+        console.log("Test start");
+        for await (const change of entryChange) {
+            console.log({ change })
+            changes.push(change);
+        }
+        console.log("Test end");
+    }
+
+    route("/route",  () => {
+        console.log("/route");
+        return <Test />
+    });
+
+    // This is detaching the promise from the route transition.
+    then(node => void descendants(node).catch(error => error))
+
+    await navigation.navigate("/route").finished;
+
+    const initialEntry = navigation.currentEntry;
+
+    navigation.updateCurrentEntry({
+        state: "Test 1"
+    });
+    navigation.updateCurrentEntry({
+        state: "Test 2"
+    });
+
+    await new Promise<void>(queueMicrotask);
+
+    navigation.updateCurrentEntry({
+        state: "Test 3"
+    });
+
+    await new Promise(resolve => setTimeout(resolve, 100));
+
+    await navigation.navigate("/another").finished;
+
+    const nextEntry = navigation.currentEntry;
+
+    // Should be setting the next entry
+    navigation.updateCurrentEntry({
+        state: "Test 4"
+    });
+
+    ok(nextEntry.id !== initialEntry.id);
+    ok(nextEntry.getState() !== initialEntry.getState());
+
+    {
+        ok(changes.length === 3);
+        ok(changes[0] === "Test 1");
+        ok(changes[1] === "Test 2");
+        ok(changes[2] === "Test 3");
+    }
+
+    await navigation.navigate("/test").finished;
+
+    {
+        ok(changes.length === 3);
+    }
+
+    console.log("Navigating to /route");
+
+    await navigation.navigate("/route").finished;
+
+    navigation.updateCurrentEntry({
+        state: "Test 5"
+    });
+    navigation.updateCurrentEntry({
+        state: "Test 6"
+    });
+
+    navigation.updateCurrentEntry({
+        state: "Test 7"
+    });
+
+    await new Promise(resolve => setTimeout(resolve, 100));
+
+    await navigation.navigate("/").finished;
+
+    console.log(changes);
+    {
+        ok(changes.length > 3);
+        ok(changes.at(-1) === "Test 7");
+    }
 
 }
