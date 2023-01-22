53bb7408d9e6d2b9fd817970a349adb7d6a50484
Florian Klampfer <mail@qwtel.com>
2023-01-23 00:23:08 +0700

    Commit

--- src/get-navigation.ts
+++ src/get-navigation.ts
@@ -11,5 +11,50 @@ export function getNavigation(): Navigation {
   if (navigation) {
     return navigation;
   }
-  return (navigation = new NavigationPolyfill());
+  navigation = new NavigationPolyfill()
+
+  if (typeof window !== "undefined" && window.history) {
+    const origin = typeof location === "undefined" ? "https://example.com" : location.origin;
+
+    let ignorePopState = false;
+    let ignoreCurrentEntryChange = false;
+
+    navigation.addEventListener("currententrychange", ({ navigationType, from }) => {
+      if (ignoreCurrentEntryChange) return ignoreCurrentEntryChange = false;
+
+      const { currentEntry } = navigation;
+      if (!currentEntry?.sameDocument) return;
+      const url = new URL(currentEntry.url, origin);
+      const { key } = currentEntry;
+      const state = currentEntry.getState<any>() ?? {};
+      state["__key__"] = key;
+
+      switch(navigationType) {
+        case "push":
+          return history.pushState(state, "", url)
+        case "replace":
+          return history.replaceState(state, "", url)
+        case "traverse":
+          const entries = navigation.entries();
+          const fromIdx = entries.findIndex(e => e === from)
+          const currIdx = entries.findIndex(e => e === currentEntry)
+          const delta = currIdx - fromIdx;
+          ignorePopState = true
+          return history.go(delta);
+        case "reload":
+          return console.warn("TBD")
+      }
+    });
+    window.addEventListener("popstate", (ev) => {
+      if (ignorePopState) return ignorePopState = false;
+
+      const { __key__: key } = ev.state ?? {};
+      if (key) {
+        ignoreCurrentEntryChange = true;
+        navigation.traverseTo(key)
+      }
+    })
+  }
+
+  return navigation;
 }
