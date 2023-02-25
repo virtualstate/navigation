97181cb1c4555ae6e0373a519bc7aeb79379daf9
Fabian Cook <hello@fabiancook.dev>
2023-02-25 15:40:17 +1300

    Add fallback for external dependency

--- src/util/structured-json.ts
+++ src/util/structured-json.ts
@@ -1,1 +1,11 @@
 export const { stringify, parse } = await import("@ungap/structured-clone/json")
+    .catch(structuredCloneFallback)
+
+function structuredCloneFallback() {
+    const stringify = JSON.stringify.bind(JSON),
+        parse = JSON.parse.bind(JSON);
+    return {
+        stringify,
+        parse
+    };
+}
