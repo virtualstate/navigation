0530e327af27949e0052c5839a36eb6aa8f48068
Fabian Cook <hello@fabiancook.dev>
2023-03-16 23:44:42 +1300

    intercept is single use instead of repeating

--- src/await/index.ts
+++ src/await/index.ts
@@ -1,20 +1,16 @@
-import {NavigateEvent, Navigation, NavigationInterceptOptions} from "../spec/navigation";
-import {getNavigation} from "../get-navigation";
-import {createNavigationPromise, createRepeatingPromise} from "./create-promise";
+import { NavigateEvent, Navigation, NavigationInterceptOptions } from "../spec/navigation";
+import { getNavigation } from "../get-navigation";
+import { createNavigationPromise } from "./create-promise";
 
 export * from "./events";
 export * from "./create-promise";
 
-export async function intercept<S>(options?: NavigationInterceptOptions<S>, navigation: Navigation<S> = getNavigation()) {
-    return createRepeatingPromise(createInterceptPromise);
-
-    function createInterceptPromise() {
-        return createNavigationPromise(
-            "navigate",
-            navigation,
-            onNavigate
-        )
-    }
+export function intercept<S>(options?: NavigationInterceptOptions<S>, navigation: Navigation<S> = getNavigation()) {
+    return createNavigationPromise(
+        "navigate",
+        navigation,
+        onNavigate
+    );
 
     function onNavigate(event: NavigateEvent) {
         event.intercept(options);
