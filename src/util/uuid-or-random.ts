c1bb5a08a990b71aab9aec45b44c40e73bb8c95b
Rob Eisenberg <rob@bluespire.com>
2024-05-08 16:09:11 -0400

    fix: remove platform-specific uuid code to prevent bundler errors
    The Web Crypto API is available in all browsers, Node.js, and Deno. This PR assumes that covers the majority of the environments where this polyfill will be used. A simple semi-random v4 formatted string is provided for all other cases. These changes reduce the code and complexity, and should resolve bundler errors stemming from the platform-specific imports and top-level awaits that have now been removed.

--- src/util/uuid-or-random.ts
+++ src/util/uuid-or-random.ts
@@ -1,45 +1,10 @@
+const isWebCryptoSupported = "crypto" in globalThis && typeof globalThis.crypto.randomUUID === "function";
 
-const fakeUUID = {
-    v4(): string {
-        return Array
-            .from(
-                { length: 5 },
-                () => `${Math.random()}`.replace(/^0\./, "")
-            )
-            .join("-")
-            .replace(".", "");
-    },
-}
-
-async function getImportUUIDOrNodeRandomUUID() {
-    const { v4 } = await import("./import-uuid")
-        .catch(async () => {
-            // @ts-ignore
-            const crypto: Crypto = await import("node:crypto");
-            return {
-                v4() {
-                    return crypto.randomUUID()
-                }
-            }
-        })
-        .catch(async () => import("uuid"))
-        .catch(() => undefined)
-        .then(
-            (mod) =>
-                mod ?? fakeUUID
-        );
-    return { v4 };
-}
-
-/** post rollup replace importUuid **/
-const uuidModule = (
-    await getImportUUIDOrNodeRandomUUID()
-);
-const getUuidModule = () => uuidModule;
-/** post rollup replace importUuid **/
-
-export function v4() {
-    const uuidModule = getUuidModule();
-    if (!(uuidModule?.v4)) return fakeUUID.v4();
-    return uuidModule.v4();
-}
+export const v4: () => string = isWebCryptoSupported
+    ? globalThis.crypto.randomUUID
+    : () => Array.from(
+            { length: 5 },
+            () => `${Math.random()}`.replace(/^0\./, "")
+        )
+        .join("-")
+        .replace(".", "");
