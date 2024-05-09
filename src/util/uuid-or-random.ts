79c1242fccba614328a2c6f7ff448f77e4a6e5cc
Fabian Cook <hello@fabiancook.dev>
2024-05-09 23:11:18 +1200

    Update src/util/uuid-or-random.ts

--- src/util/uuid-or-random.ts
+++ src/util/uuid-or-random.ts
@@ -1,7 +1,7 @@
 const isWebCryptoSupported = "crypto" in globalThis && typeof globalThis.crypto.randomUUID === "function";
 
 export const v4: () => string = isWebCryptoSupported
-    ? globalThis.crypto.randomUUID
+    ? globalThis.crypto.randomUUID.bind(globalThis.crypto)
     : () => Array.from(
             { length: 5 },
             () => `${Math.random()}`.replace(/^0\./, "")
